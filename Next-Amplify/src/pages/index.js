f7a5412a14f2fda4afc8830e52c28a505537a4ed
raviteja <ravitejamerugu@gmail.com>
2023-08-01 20:08:12 +0530

    second commit

--- Next-Amplify/src/pages/index.js
+++ Next-Amplify/src/pages/index.js
@@ -128,7 +128,7 @@ export default function About({ formattedDate }) {
   return (
     <>
       <h1>About page</h1>
-      <p>This is the about page. It was last updated on {formattedDate}.</p>
+      <p>This is the about page. It was last updated on the -->  {formattedDate}.</p>
       <p>
         <a href="/products">View a server-side rendered page.</a>
       </p>
@@ -137,7 +137,7 @@ export default function About({ formattedDate }) {
 }
 
 export async function getStaticProps() {
-  const lastUpdateDate = new Date(2022, 9, 1); // Set the last update date
+  const lastUpdateDate = new Date(2023, 8, 1); // Set the last update date
   const formattedDate = new Intl.DateTimeFormat("en-US", {
     dateStyle: "long",
     timeStyle: "long",
